f0c113d2ec6740238848035d08e7555feabd2f88
Torben Albert-Lindqvist <torben223@gmail.com>
2022-03-15 20:56:28 +0100

    allow for selecting variables for comparison

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,31 +1,27 @@
+from pydoc import render_doc
 import streamlit as st
 import pandas as pd
 from data import load_data
+import numpy as np
 
 import plotly.figure_factory as ff
+import plotly.express as px
 
 if __name__ == "__main__":
     df = load_data()
 
-    x1 = df[df.gender == "male"].salary
-    x2 = df[df.gender == "female"].salary
-    x3 = df[df.gender == "no answer"].salary
+    # Only allow categorical columns
+    columns = df.select_dtypes(include=['category']).columns.values
 
-    hist_data = [x1, x2]
+    option = st.selectbox('Comparison variable', columns)
+    render_df = df.copy()
 
-    group_labels = ["Male", "Female"]
-    colors = ["#A56CC1", "#A6ACEC"]
+    # Remove unnecessary values for clearner plots
+    if option == 'received_equity':
+        render_df = render_df[~render_df.received_equity.isin(['I do not know', 'Prefer not to say'])]
+    # TODO: Implement for other columns
+    # elif 
 
-    fig = ff.create_distplot(
-        hist_data, group_labels, colors=colors, bin_size=5000, show_rug=False
-    )
-    fig.update_xaxes(range=[0, 150000])
-
-    fig.update_layout(title_text="Hist and Curve Plot")
+    fig = px.box(render_df, x=option, y="salary")
 
     st.plotly_chart(fig, use_container_width=True)
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
